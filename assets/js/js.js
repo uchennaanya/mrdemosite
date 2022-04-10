4b29cb6606f1822dfce399f09af0f5ee48dd8c8a
uchenna <uchennaongit@gmail.com>
2022-04-10 11:27:41 +0100

    Initial commit

---
+++ assets/js/js.js
@@ -0,0 +1,33 @@
+window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 500 ||
+      document.documentElement.scrollTop > 500
+    ) {
+      document.getElementById("navHeader").style.padding = "0 0.5rem 0 0";
+      document.getElementById("navHeader").style.backgroundColor =
+        "rgba(0, 0, 0, 0.9)";
+    } else {
+      document.getElementById("navHeader").style.padding = "1.5rem  2rem 1rem 0.3rem";
+      document.getElementById("navHeader").style.transition = "1s";
+      document.getElementById("navHeader").style.backgroundColor =
+        "rgba(0, 0, 0, 0)";
+    }
+  }
+
+let toggle = document.getElementById("toggle")
+let show = document.getElementById("show")
+
+toggle.addEventListener("click", () => {
+
+    if(show.classList.contains("hide")) {
+        show.classList.remove("hide")
+        show.classList.add("show")
+    } else {
+        show.classList.add("hide")
+        show.classList.remove("show")
+    }
+})
